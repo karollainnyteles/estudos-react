0a5defb615dcc27dafe83c8e83197960744267ac
TO\041281182771 <karollainnyteles@mail.uft.edu.br>
2021-05-21 16:48:43 -0300

    Início de Challenge para ciclo ativo

--- src/components/ChallengBox.tsx
+++ src/components/ChallengBox.tsx
@@ -1,15 +1,34 @@
 import styles from '../styles/components/ChallengBox.module.css'
 
 export function ChallengBox(){
+  const hasActiveChalleng = true;
   return(
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengNotActive}>
+      {hasActiveChalleng ?(
+        <div className={styles.challengActive}>
+          <header>Ganhe 400 px</header>
+          <main>
+            <img src="icons/bodt.svg"/>
+            <strong>Novo desafio</strong>
+            <p>Levante e faça alguma coisa</p>
+          </main>
+          <footer>
+            <button type="button" className={styles.challengFailedButton}>Falhei</button>
+            <button type="button" className={styles.challengSucceededButton}>Completei</button>
+          </footer>
+        </div>
+
+      ) : (
+        <div className={styles.challengNotActive}>
         <strong>Finalize um ciclo para receber um desafio</strong>
         <p>
           <img src="icons/level-up.svg" alt="Level Up"/>
           Avance de level completando desafios
         </p>
       </div>
+
+      )}
+      
     </div>
   )
 }
